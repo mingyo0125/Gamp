c79e3345ac1822002306237c1cd661d497e91c23
mingyo0125 <wjdalsry125@gmail.com>
2023-03-07 14:02:04 +0900

    2023_03_07

---
+++ 2023_03_07(Gamp)/2023_03_07(Gamp)/FileName.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int nansu;
+	srand((unsigned int)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		nansu = rand() % 21 + 10; //10 ~ 30±îÁö
+		cout << nansu << endl;
+	}
+}
