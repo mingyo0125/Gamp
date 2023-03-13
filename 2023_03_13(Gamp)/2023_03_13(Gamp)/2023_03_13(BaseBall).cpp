e19eebe32a72d2d77f6474180a59b2a49801f588
mingyo0125 <wjdalsry125@gmail.com>
2023-03-13 22:45:26 +0900

    2player

--- 2023_03_13(Gamp)/2023_03_13(Gamp)/2023_03_13(BaseBall).cpp
+++ 2023_03_13(Gamp)/2023_03_13(Gamp)/2023_03_13(BaseBall).cpp
@@ -1,103 +1,153 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main()
 {
 	int iNumber[9] = {};
-
 	for (int i = 0; i < 9; i++)
-	{
 		iNumber[i] = i + 1;
-	}
 
-	int idx1, idx2 = 0, temp = 0;
+	int idx1, idx2, temp;
 	srand((unsigned int)time(NULL));
-	//셔플
+	// 셔플을 할거야.
 	for (int i = 0; i < 100; i++)
 	{
 		idx1 = rand() % 9;
 		idx2 = rand() % 9;
-
+		//		cout << idx1 << " " << idx2 << endl;
 		temp = iNumber[idx1];
 		iNumber[idx1] = iNumber[idx2];
 		iNumber[idx2] = temp;
-		
 	}
-	//cout << " * * * " << endl;
-	cout << iNumber[0] << "\t" << iNumber[1] << "\t" << iNumber[2] << endl;
-	
+	//cout << "* * *" << endl;
+	cout << iNumber[0] << "\t" <<
+		iNumber[1] << "\t" << iNumber[2] << endl;
 	int iStrike = 0, iBall = 0;
-	int count = 1;
+	int iCnt = 1;
+	int iStrike2 = 0, iBall2 = 0;
+	int iCnt2 = 1;
+	bool isFirstP = true;
 	int iInput[3];
 
-	while (true)
-	{
-		cout << count << "회" << endl;
-		cout << "1~9까지의 숫자 3개를 입력하세요 {0 : 종료}" << endl;
-		cin >> iInput[0] >> iInput[1] >> iInput[2];
-
-		//종료조건
-		if (iInput[0] == 0 || iInput[1] == 0 || iInput[2] == 0)
+		while (true)
 		{
-			break;
-		}
-		//예외처리
-		else if (iInput[0] < 1 || iInput[0] > 9 ||
-				 iInput[1] < 1 || iInput[1] > 9 ||
-				 iInput[2] < 1 || iInput[2] > 9)
-		{
-			cout << "잘못된 숫자를 입력하였습니다. 1 ~ 9까지의 숫자를 입력하세요" << endl;
-			continue;
-		}
-		else if(iInput[0] == iInput[1] ||
-				iInput[1] == iInput[2] ||
-				iInput[2] == iInput[0])
-		{
-			cout << "잘못된 숫자를 입력하였습니다. 서로 다른 1 ~ 9까지의 숫자를 입력하세요" << endl;
-			continue;
-		}
-
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; i < 3; j++)
+			if (isFirstP)
 			{
-				if (iNumber[i] == iInput[j])
+				cout << "player 1" << endl;
+				cout << iCnt << " 회" << endl;
+				cout << "1 ~ 9 숫자 중 숫자 3개를 입력하세요.(0: 종료): ";
+				cin >> iInput[0] >> iInput[1] >> iInput[2];
+
+				// 종료 조건
+				if (iInput[0] == 0 || iInput[1] == 0 || iInput[2] == 0)
+					break;
+
+				// 예외 처리1
+				else if (iInput[0] < 1 || iInput[0] > 9 ||
+					iInput[1] < 1 || iInput[1] > 9 ||
+					iInput[2] < 1 || iInput[2] > 9)
 				{
-					if (i == j)
+					cout << "잘못된 숫자를 입력하였습니다. 1~9 범위의 숫자를 입력하세요." << endl;
+					continue;
+				}
+				// 예외 처리2
+				else if (iInput[0] == iInput[1] ||
+					iInput[1] == iInput[2] ||
+					iInput[2] == iInput[0])
+				{
+					cout << "중복된 숫자를 입력하였습니다. 금집니다 금지!!." << endl;
+					continue;
+				}
+				// 여기서부터 자율적으로 짜기
+				iStrike = iBall = 0;
+				for (int i = 0; i < 3; i++)
+				{
+					for (int j = 0; j < 3; j++)
 					{
-						iStrike++;
-					}
-					else
-					{
-						iBall++;
+						if (iNumber[i] == iInput[j])
+						{
+							if (i == j)
+								iStrike++;
+							else
+								iBall++;
+							//strike, ball
+						}
 					}
 				}
+
+				if (iStrike == 3)
+				{
+					system("cls"); // 콘솔 화면 지우기
+					cout << "숫자를 모두 맞췄습니다 축하합니다 ㅎㅎ" << endl;
+					break;
+				}
+				else if (iStrike == 0 && iBall == 0)
+					cout << "Out입니다! " << endl;
+				else
+					system("cls"); // 콘솔 화면 지우기
+				cout << iStrike << "strike " << iBall << "ball" << endl;
+				isFirstP = (isFirstP == true) ? false : true;
+				iCnt++;
+			}
+			else
+			{
+				cout << "player 2" << endl;
+				cout << iCnt2 << " 회" << endl;
+				cout << "1 ~ 9 숫자 중 숫자 3개를 입력하세요.(0: 종료): ";
+				cin >> iInput[0] >> iInput[1] >> iInput[2];
+
+				// 종료 조건
+				if (iInput[0] == 0 || iInput[1] == 0 || iInput[2] == 0)
+					break;
+
+				// 예외 처리1
+				else if (iInput[0] < 1 || iInput[0] > 9 ||
+					iInput[1] < 1 || iInput[1] > 9 ||
+					iInput[2] < 1 || iInput[2] > 9)
+				{
+					cout << "잘못된 숫자를 입력하였습니다. 1~9 범위의 숫자를 입력하세요." << endl;
+					continue;
+				}
+				// 예외 처리2
+				else if (iInput[0] == iInput[1] ||
+					iInput[1] == iInput[2] ||
+					iInput[2] == iInput[0])
+				{
+					cout << "중복된 숫자를 입력하였습니다. 금집니다 금지!!." << endl;
+					continue;
+				}
+				// 여기서부터 자율적으로 짜기
+				iStrike2 = iBall2 = 0;
+				for (int i = 0; i < 3; i++)
+				{
+					for (int j = 0; j < 3; j++)
+					{
+						if (iNumber[i] == iInput[j])
+						{
+							if (i == j)
+								iStrike2++;
+							else
+								iBall2++;
+							//strike, ball
+						}
+					}
+				}
+
+				if (iStrike2 == 3)
+				{
+					system("cls"); // 콘솔 화면 지우기
+					cout << "숫자를 모두 맞췄습니다 축하합니다 ㅎㅎ" << endl;
+					break;
+				}
+				else if (iStrike2 == 0 && iBall2 == 0)
+					cout << "Out입니다! " << endl;
+				else
+					system("cls"); // 콘솔 화면 지우기
+				cout << iStrike2 << "strike " << iBall2 << "ball" << endl;
+				isFirstP = (isFirstP == true) ? false : true;
+				iCnt2++;
+			}
 			}
 		}
-
-		//정답
-		if (iStrike == 3)
-		{
-			cout << "정답입니다";
-			break;
-		}
-		else if(iStrike == 0 || iBall == 0)
-		{
-			cout << "바보";
-			count++;
-			continue;
-		}
-		else
-		{
-			count++;
-			cout << "스트라이크 : " << iStrike << endl;
-			cout << "볼 : " << iBall << endl;
-			continue;
-		}
-
-	}
-
-	//여기서부터 자율적으로 짜기
-
-	//system("cls"); //콘솔화면 지우기
-}
+		
+	
--- 2023_03_13(Gamp)/2023_03_13(Gamp)/2023_03_13(StarPos).cpp
+++ 2023_03_13(Gamp)/2023_03_13(Gamp)/2023_03_13(StarPos).cpp
@@ -1,73 +1,38 @@
-//#include <iostream>
+//#include<iostream>
 //using namespace std;
 //
 //int main()
 //{
-//	cout << "=================" << endl;
-//	cout << "스타포스 강화 게임" << endl;
-//	cout << "=================" << endl << endl;
-//
+//	cout << " ================= " << endl;
+//	cout << " 스타포스 강화 게임" << endl;
+//	cout << " ================= " << endl;
 //	int iUpgrade = 0;
-//
-//	cout << "현재 무기는 몇성인가요? : ";
+//	cout << "현재 무기는 몇성인가요?: ";
 //	cin >> iUpgrade;
-//
-//	float fPercent;
-//	//0~2성 : 90%, 3~5성 50%, 6~9성 10%, 10성 이상 1%
-//
 //	srand((unsigned int)time(NULL));
-//	fPercent = rand() % 10001 / 100.f; //100.f = float형 변환
+//	float fPercent = rand() % 10001 / 100.f;
+//	cout << fPercent;
 //
-//	cout << "현재 무기 Upgrade : " << iUpgrade << "성" << endl;
-//	cout << "현재 무기 Percent : " << fPercent << "%" << endl;
-//	cout << "강화하시겠습니까? 예 : 1, 아니오 : 0" << endl;
+//	// 0~2성: 90% 확률, 3~5성: 50%, 6~9성: 10%, 10성↑: 1% 확률로 성공
+//	cout << "현재 무기 Upgrade: " << iUpgrade << "성" << endl;
+//	cout << "강화 확률 Percent: " << fPercent << "%" << endl;
+//	cout << "강화하시겠습니까? 예: 1, 아니오: 0" << endl;
 //
-//	int iInput;
+//	int iInput; 
 //	cin >> iInput;
 //
-//	if (iInput == 0) { return 0; }
-//
-//	if (iUpgrade <= 2)
-//	{
+//	if (iInput == 0)
+//		return 0;
+//	if (iUpgrade <= 2) // 90% 이기를 원해.
 //		if (fPercent <= 90.f)
-//		{
-//			cout << "강화성공" << endl;
-//		}
+//			cout << "강화 성공" << endl;
 //		else
-//		{
-//			cout << "강화실패" << endl;
-//		}
-//	}
-//	else if (iUpgrade <= 5)
-//	{
-//		if (fPercent <= 50.f)
-//		{
-//			cout << "강화성공" << endl;
-//		}
+//			cout << "강화 실패" << endl;
+//	if (iUpgrade <= 2) // 90% 이기를 원해.
+//		if (fPercent <= 90.f)
+//			cout << "강화 성공" << endl;
 //		else
-//		{
-//			cout << "강화실패" << endl;
-//		}
-//	}
-//	else if (iUpgrade <= 9)
-//	{
-//		if (fPercent <= 10.f)
-//		{
-//			cout << "강화성공" << endl;
-//		}
-//		else
-//		{
-//			cout << "강화실패" << endl;
-//		}
-//	}
-//	else
-//	{
-//		if (fPercent <= 1.f)
-//		{
-//			cout << "강화성공" << endl;
-//		}
-//		else
-//		{
-//			cout << "강화실패" << endl;
-//		}
-//	}
+//			cout << "강화 실패" << endl;
+//
+//
+//}
