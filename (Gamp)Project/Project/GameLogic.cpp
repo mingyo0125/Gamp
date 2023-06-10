51d4664a0f78e58d9b64b7ee49f9e96fb15ebd14
mingyo0125 <wjdalsry125@gmail.com>
2023-06-11 00:13:11 +0900

    뭐한지모르겠음이것저것한듯

--- (Gamp)Project/Project/GameLogic.cpp
+++ (Gamp)Project/Project/GameLogic.cpp
@@ -145,9 +145,9 @@ void MakeItem(char item, char _map[VERTICAL][HORIZON])
 	horiRand = rand() % HORIZON;
 
 	verRand = std::clamp(verRand, 1, 28);
-	horiRand = std::clamp(horiRand, 1, 17);
+	horiRand = std::clamp(horiRand, 1, 18);
 
-	_map[verRand][horiRand] = item;
+	_map[horiRand][verRand] = item;
 }
  
 
